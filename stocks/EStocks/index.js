675bc4988786ef8941ec7beede1b6fea2b0cf352
csmurillo <csmurillo00@gmail.com>
2022-08-09 01:11:01 -0700

    updating

--- stocks/EStocks/index.js
+++ stocks/EStocks/index.js
@@ -5,8 +5,8 @@ const { randomLivePrice }=require('../../helper/randomLivePrice');
 
 function listELivePrice(){
     // testing
-    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Africa/Casablanca' });
-    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Africa/Casablanca' });
+    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Istanbul' });
+    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Istanbul' });
     const liveTime = new Date(newYorkDate);
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
     // const liveTime = new Date(newYorkDate);
@@ -55,7 +55,7 @@ function listELivePrice(){
 
 function listEStocksDayHistory(){
     // testing
-    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Africa/Casablanca' });
+    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Istanbul' });
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Japan' });
     const liveTime = new Date(newYorkDate);
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
--- stocks/RStocks/index.js
+++ stocks/RStocks/index.js
@@ -5,8 +5,8 @@ const { randomLivePrice }=require('../../helper/randomLivePrice');
 
 function listRLivePrice(){
     // testing
-    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Africa/Casablanca' });
-    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Africa/Casablanca' });
+    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Istanbul' });
+    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Istanbul' });
     const liveTime = new Date(newYorkDate);
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
     // const liveTime = new Date(newYorkDate);
@@ -55,7 +55,7 @@ function listRLivePrice(){
 
 function listRStocksDayHistory(){
     // testing
-   const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Africa/Casablanca' });
+   const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Istanbul' });
    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Japan' });
    const liveTime = new Date(newYorkDate);
    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
