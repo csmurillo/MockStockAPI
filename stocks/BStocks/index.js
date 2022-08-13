c33efe98c4f9141c9d02e790166fc3beaf85907d
csmurillo <csmurillo00@gmail.com>
2022-08-13 10:11:17 -0700

    heroku updating

--- stocks/BStocks/index.js
+++ stocks/BStocks/index.js
@@ -5,8 +5,8 @@ const { randomLivePrice }=require('../../helper/randomLivePrice');
 
 function listBLivePrice(){
     // testing
-    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Australia/Sydney' });
-    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Australia/Sydney' });
+    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Tegucigalpa' });
+    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Tegucigalpa' });
     const liveTime = new Date(newYorkDate);
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
     // const liveTime = new Date(newYorkDate);
@@ -56,7 +56,7 @@ function listBLivePrice(){
 function listBStocksDayHistory(){
     
     // testing
-    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Australia/Sydney' });
+    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Tegucigalpa' });
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Japan' });
     const liveTime = new Date(newYorkDate);
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
--- stocks/WStocks/index.js
+++ stocks/WStocks/index.js
@@ -5,8 +5,8 @@ const { randomLivePrice }=require('../../helper/randomLivePrice');
 
 function listWLivePrice(){
     // testing
-    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Australia/Sydney' });
-    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Australia/Sydney' });
+    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Tegucigalpa' });
+    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Tegucigalpa' });
     const liveTime = new Date(newYorkDate);
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
     // const liveTime = new Date(newYorkDate);
@@ -55,7 +55,7 @@ function listWLivePrice(){
 
 function listWStocksDayHistory(){
            // testing
-   const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Australia/Sydney' });
+   const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Tegucigalpa' });
    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Japan' });
    const liveTime = new Date(newYorkDate);
    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
