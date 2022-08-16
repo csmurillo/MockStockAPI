41d0b35fa56bd550e3ca829f27541db06ff5ec57
csmurillo <csmurillo00@gmail.com>
2022-08-16 00:58:23 -0700

    heroku update

--- stocks/IStocks/index.js
+++ stocks/IStocks/index.js
@@ -5,8 +5,8 @@ const { randomLivePrice }=require('../../helper/randomLivePrice');
 
 function listILivePrice(){
      // testing
-     const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Pacific/Auckland' });
-     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Pacific/Auckland' });
+     const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Europe/Madrid' });
+     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Europe/Madrid' });
      const liveTime = new Date(newYorkDate);
      // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
      // const liveTime = new Date(newYorkDate);
@@ -55,7 +55,7 @@ function listILivePrice(){
 
 function listIStocksDayHistory(){
     // testing
-    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Pacific/Auckland' });
+    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Europe/Madrid' });
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Japan' });
     const liveTime = new Date(newYorkDate);
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
