bde2be92c9738fd15eb5da4ffe1dd965863d6ef2
csmurillo <csmurillo00@gmail.com>
2022-08-22 14:21:12 -0700

    heroky update

--- stocks/VStocks/index.js
+++ stocks/VStocks/index.js
@@ -5,8 +5,8 @@ const { randomLivePrice }=require('../../helper/randomLivePrice');
 
 function listVLivePrice(){
     // testing
-    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Singapore' });
-    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Singapore' });
+    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Pacific/Auckland' });
+    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Pacific/Auckland' });
     const liveTime = new Date(newYorkDate);
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
     // const liveTime = new Date(newYorkDate);
@@ -55,7 +55,7 @@ function listVLivePrice(){
 
 function listVStocksDayHistory(){
           // testing
-   const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Singapore' });
+   const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Pacific/Auckland' });
    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Japan' });
    const liveTime = new Date(newYorkDate);
    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
