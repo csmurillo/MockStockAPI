f8537b8c069a4f82ca6a2d3a0b6893f5c18e45c2
csmurillo <csmurillo00@gmail.com>
2022-08-30 09:24:33 -0700

    heroky ypudate

--- stocks/KStocks/index.js
+++ stocks/KStocks/index.js
@@ -5,8 +5,8 @@ const { randomLivePrice }=require('../../helper/randomLivePrice');
 
 function listKLivePrice(){
     // testing
-    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Europe/Madrid' });
-    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Europe/Madrid' });
+    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
+    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
     const liveTime = new Date(newYorkDate);
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
     // const liveTime = new Date(newYorkDate);
@@ -55,7 +55,7 @@ function listKLivePrice(){
 
 function listKStocksDayHistory(){
      // testing
-     const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Europe/Madrid' });
+     const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
      // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Japan' });
      const liveTime = new Date(newYorkDate);
      // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
