32d571cba8e8efbcb67237d71bec9544ecc2f6df
csmurillo <csmurillo00@gmail.com>
2022-09-01 11:01:24 -0700

    heroky update

--- stocks/ZStocks/index.js
+++ stocks/ZStocks/index.js
@@ -5,8 +5,8 @@ const { randomLivePrice }=require('../../helper/randomLivePrice');
 
 function listZLivePrice(){
     // testing
-    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
-    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
+    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
+    // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
     const liveTime = new Date(newYorkDate);
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
     // const liveTime = new Date(newYorkDate);
@@ -55,7 +55,7 @@ function listZLivePrice(){
 
 function listZStocksDayHistory(){
     // testing
-    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
+    const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/Los_Angeles' });
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'Japan' });
     const liveTime = new Date(newYorkDate);
     // const newYorkDate = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
